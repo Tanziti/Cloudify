f13dbd9e9121ab4f59e6a79681d11424aaeeb81a
tanziti <tanzitian3@gmail.com>
2023-09-14 14:29:13 -0400

    setup2

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,12 @@ module Cloudify
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use ActonDispatch::Session::CookierStore,
+    key: '_appname_session',
+    same_site: :lax,
+    secure: Rails.env.production?
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
